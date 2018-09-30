76b1cea8f2b3b9f3755ddd3d12a785e60e92469b
Tiago Chaves <tiagoloureirochaves@gmail.com>
2018-09-30 14:39:14 -0300

    Improve tridiagonal_lu time complexity

--- tridiagonal-lu-decompostion.py
+++ tridiagonal-lu-decompostion.py
@@ -4,7 +4,7 @@ from numpy import dot
 def diagonals(A):
   n = len(A)
   a = [0.0 for i in range(n-1)] # lower diagonal (i-j==1)
-  b = [0.0 for i in range(n)]   # main  diagonal (i==j)
+  b = [0.0 for i in range(n)]   # main  diagonal  (i==j)
   c = [0.0 for i in range(n-1)] # upper diagonal (j-i==1)
   c[0] = A[0][1]
   b[0] = A[0][0]
@@ -22,13 +22,11 @@ def tridiagonal_lu(A):
     L = [[(1.0 if i == j else 0.0) for i in range(n)] for j in range(n)]
     U = [[(1.0 if i == j else 0.0) for i in range(n)] for j in range(n)]
     for i in range(n-1):
-      for j in range(n):
-        if j - i == 1.0:
-          U[i][j] = c[i]
+      U[i][i+1] = c[i]
     
     U[1][1] = b[1]
     for k in range(1, n):
-      L[k][k-1] = a[k-1]/U[k-1][k-1] # a[k-1] is actually a_k since it starts at a_2
+      L[k][k-1] = a[k-1]/U[k-1][k-1] # we use a[k-1] instead of a[k] since a starts at a_2
       U[k][k] = b[k] - L[k][k-1]*c[k-1]
     
     return (L, U)
@@ -52,11 +50,3 @@ def tridiagonal_decompose_and_solve(A, b):
     y = forward_substitute(L, b)
     x = back_substitute(U, y)
     return (L, U, y, x)
-    return (L, U)
-
-A = [[1, 2, 0, 0], [2, 2, 2, 0], [0, 1, 1, 4], [0, 0, 7, 4]]
-# diagonals(A) = [[2, 1, 7], [1, 2, 1, 3], [2, 2, 4]]
-b = [3, 6, 6, 11]
-for part in tridiagonal_decompose_and_solve(A, b):
-    pprint(part, width=30)
-    print
